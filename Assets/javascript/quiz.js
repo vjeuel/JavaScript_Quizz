97ee836150454527eb28984a039b572b8be5e4cb
vjeuel <vjeuel@gmail.com>
2020-04-21 06:47:26 -0500

    Questions and answers added to page, but just the object showing

--- Assets/javascript/quiz.js
+++ Assets/javascript/quiz.js
@@ -1,12 +1,35 @@
+
+// ----------  QUIZ QUESTIONS  ---------- //
+var questions = [
+   {
+      question: "What's the meaning of JS?",
+      answers: ["JavaScript", "JadeServer", "JundleSate", "JSlate"],
+      correct: "JavaScript"
+   },{
+      question: "JavaScript file has an extension of?",
+      answers: [".java", ".js", ".javascript", ".xml"],
+      correct: ".js"
+   },{
+      question: "var numbers = [1,2,3] is an example of:",
+      answers: ["Array", "Function", "Object", "Method"],
+      correct: "Array"
+   }, {
+      question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+      answers: ["Event", "RegExp", "Boolean", "Condition"],
+      correct: "Boolean"
+   }
+];
+
 // ----------  MAIN VARIABLES  ---------- //
-const readyButtonElement = document.getElementById("ready-button");
-const quizBoxElement = document.getElementById("quiz-box");
-const questionElement = document.getElementById("questions");
-const answerBtnsElement = document.getElementById("answer-btns");
+var readyButtonElement = document.getElementById("ready-button");
+var quizBoxElement = document.getElementById("quiz-box");
+var questionElement = document.getElementById("questions");
+var quizQuestionElement = document.getElementById("quizQuestion");
+// var answerBtnsElement = document.getElementById("answer-btns");
 
-let mixedQuestions, currentQuestion;
+var mixedQuestions, currentQuestion;
 
-
+var introTextElement = document.getElementById("intro");
 
 
 // ----------  TIMER  ---------- //
@@ -26,48 +49,44 @@ const countdown = document.getElementById("ready-button").onclick = function cou
 window.onload = function hideAnswerButtons() {
    document.getElementById("answer-btns").style.display = "none";
    document.getElementById("timer").style.display = "none";
-   
+   introTextElement.classList.remove("hide");
+};
+
+
+function startGame() {
+   quizQuestionElement.classList.remove("hide");   
+   readyButtonElement.classList.add("hide");
+   introTextElement.classList.add("hide");
+   document.getElementById("answer-btns").style.display = "";
+   document.getElementById("timer").style.display = "";
 };
 
 readyButtonElement.addEventListener("click", startGame);
 
-function startGame() {
-   readyButtonElement.classList.add("hide");
-   // document.getElementById("intro").style.display = "none";
-   document.getElementById("answer-btns").style.display = "";
-   document.getElementById("timer").style.display = "";
-   mixedQuestions = questions.sort(() => Math.random() - 0.5);
-   currentQuestion = 0;
-};
 
 // ----------  CONTINUE GAME  ---------- //
-function nextQuestion() {
-   showQuestion(mixedQuestions[currentQuestion]);
+function showQuestion() {
+   var option1Element = document.getElementById("option1");
+   var option2Element = document.getElementById("option2");
+   var option3Element = document.getElementById("option3");
+   var option4Element = document.getElementById("option4");
+   
+   // ----------  Questions showing in the Quiz  ---------- //
+   document.getElementById("quizQuestion").innerHTML += questions[0];
+
+   
+   // ----------  Options of answers showing in the Quiz  ---------- //
+   document.getElementById("option1").innerHTML += questions;
+   document.getElementById("option2").innerHTML += questions;
+   document.getElementById("option3").innerHTML += questions;
+   document.getElementById("option4").innerHTML += questions;
+   
+   
+
+   console.log("test");
 };
 
-
-
-function showQuestion() {
-   let q = questions[currentQuestion];
-   
-   question.innerHTML = "<p>" + q.question +"</p>";
-   option1.innerHTML = q.option1;
-   option2.innerHTML = q.option2;
-   option3.innerHTML = q.option3;
-   option4.innerHTML = q.option4;
-   
-   // question.answers.forEach(answer => {
-      //    const button = document.createElement("button");
-      //    button.innerText = answer.text;
-      //    button.classList.add("btn");
-      //    if (answer.correct) {
-         //       button.dataset.correct = answer.correct;
-         //    };
-         //    button.addEventListener("click", pickAnswer);
-         //    answerBtnsElement.appendChild(button);
-         // });
-      };
-      
+showQuestion();
       
 // ----------  Check ANSWERS  ---------- //
 function checkAnswer(answer) {
@@ -90,88 +109,3 @@ function answerIsWrong() {
 function pickAnswer() { 
 
 };
-
-
-
-
-
-
-// ----------  QUESTIONS  ---------- //
-// function questions(question, answers, correct) {
-//    this.quizQuestion = question;
-//    this.quizAnswers = answers;
-//    this.quizCorrect = correct;
-// }
-
-let questions = [
-   {
-      question: "What's the meaning of JS?",
-      option1: "JavaScript",
-      option2: "JadeServer",
-      option3: "JundleSate",
-      option4: "JSlate",
-      correct: "A"
-   }, {
-      question: "JavaScript file has an extension of?",
-      option1: ".java",
-      option2: ".js",
-      option3: ".javascript",
-      option4: ".xml",
-      correct: "B"
-   }, {
-      question: "var numbers = [1,2,3] is an example of:",
-      option1: "Array",
-      option2: "Function",
-      option3: "Object",
-      option4: "Method",
-      correct: "A"
-   }, {
-      question: "What is a JavaScript element that represents either TRUE or FALSE values?",
-      option1: "Event",
-      option2: "RegExp",
-      option3: "Boolean",
-      option4: "Condition",
-      correct: "C"
-   }
-];
-
-console.log(questions);
-
-// let questions = [
-//    {
-//       question: "What's the meaning of JS?",
-//       answers: [
-//          { text: "JavaScript", correct: true },
-//          { text: "JadeServer", correct: false },
-//          { text: "JundleSate", correct: false },
-//          { text: "JSlate", correct: false },
-//       ]
-//    },
-//    {
-//       question: "JavaScript file has an extension of?",
-//       answers: [
-//          { text: ".java", correct: false },
-//          { text: ".js", correct: true },
-//          { text: ".javascript", correct: false },
-//          { text: ".xml", correct: false },
-//       ]
-//    },
-//    {
-//       question: "var numbers = [1,2,3] is an example of:",
-//       answers: [
-//          { text: "Array", correct: true },
-//          { text: "Function", correct: false },
-//          { text: "Object", correct: false },
-//          { text: "Method", correct: false },
-//       ]
-//    },
-//    {
-//       question: "What is a JavaScript element that represents either TRUE or FALSE values?",
-//       answers: [
-//          { text: "Event", correct: false },
-//          { text: "RegExp", correct: false },
-//          { text: "Boolean", correct: true },
-//          { text: "Condition", correct: false },
-//       ]
-//    },
-// ];
