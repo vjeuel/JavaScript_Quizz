bc1138ad2120dc8cc3e9cfe39ce1a97542ee3417
vjeuel <vjeuel@gmail.com>
2020-04-21 17:48:15 -0500

    10 seconds punishement added

--- Assets/javascript/quiz.js
+++ Assets/javascript/quiz.js
@@ -5,15 +5,18 @@ var questions = [
       question: "What's the meaning of JS?",
       answers: ["JavaScript", "JadeServer", "JundleSate", "JSlate"],
       correct: "JavaScript"
-   },{
+   },
+   {
       question: "JavaScript file has an extension of?",
       answers: [".java", ".js", ".javascript", ".xml"],
       correct: ".js"
-   },{
+   },
+   {
       question: "var numbers = [1,2,3] is an example of:",
       answers: ["Array", "Function", "Object", "Method"],
       correct: "Array"
-   }, {
+   },
+   {
       question: "What is a JavaScript element that represents either TRUE or FALSE values?",
       answers: ["Event", "RegExp", "Boolean", "Condition"],
       correct: "Boolean"
@@ -25,12 +28,11 @@ var readyButtonElement = document.getElementById("ready-button");
 var quizBoxElement = document.getElementById("quiz-box");
 var questionElement = document.getElementById("questions");
 var quizQuestionElement = document.getElementById("quizQuestion");
-// var answerBtnsElement = document.getElementById("answer-btns");
-
-var mixedQuestions, currentQuestion;
+var currentQuestionIndex = 0;
+var optionElement = document.getElementById("answer-btns");
 
 var introTextElement = document.getElementById("intro");
-
+var quizTitleElement = document.getElementById("question-title");
 
 // ----------  TIMER  ---------- //
 var remainTime = 90;
@@ -54,6 +56,8 @@ window.onload = function hideAnswerButtons() {
 
 
 function startGame() {
+   console.log("start")
+
    quizQuestionElement.classList.remove("hide");   
    readyButtonElement.classList.add("hide");
    introTextElement.classList.add("hide");
@@ -64,29 +68,54 @@ function startGame() {
 readyButtonElement.addEventListener("click", startGame);
 
 
+
 // ----------  CONTINUE GAME  ---------- //
 function showQuestion() {
-   var option1Element = document.getElementById("option1");
-   var option2Element = document.getElementById("option2");
-   var option3Element = document.getElementById("option3");
-   var option4Element = document.getElementById("option4");
+   var currentQuestion = questions[currentQuestionIndex];
    
-   // ----------  Questions showing in the Quiz  ---------- //
-   document.getElementById("quizQuestion").innerHTML += questions[0];
-
+   optionElement.innerHTML = "";
    
-   // ----------  Options of answers showing in the Quiz  ---------- //
-   document.getElementById("option1").innerHTML += questions;
-   document.getElementById("option2").innerHTML += questions;
-   document.getElementById("option3").innerHTML += questions;
-   document.getElementById("option4").innerHTML += questions;
-   
-   
-
-   console.log("test");
+   quizTitleElement.textContent = currentQuestion.question;
+   currentQuestion.answers.forEach(function (choice, i) {
+      var optionNode = document.createElement("button");
+      optionNode.setAttribute("class", "btn");
+      optionNode.setAttribute("value", choice);
+      optionNode.textContent = choice;
+      optionNode.onclick = questionClick;
+      
+      optionElement.appendChild(optionNode);
+      
+      function questionClick() {
+         if (this.value == questions[currentQuestionIndex].correct) {
+            console.log("correct");
+            
+            currentQuestionIndex++;
+         } else {
+            if (remainTime > 10) {
+               remainTime -= 10;
+            } else {
+               remainTime = 1;
+            }
+            console.log("incorrect");
+            currentQuestionIndex++;
+            
+         }
+         console.log(currentQuestionIndex)
+         showQuestion();
+      };
+   });
 };
 
 showQuestion();
+
+// ----------  GAME OVER  ---------- //
+function gameOver() {
+   if (remainTime == 0) {
+      window.location.href = "gameover.html";
+   }
+   
+};
+
       
 // ----------  Check ANSWERS  ---------- //
 function checkAnswer(answer) {
@@ -99,7 +128,7 @@ function checkAnswer(answer) {
 
 // ----------  ANSWER IS CORRECT OR WRONG  ---------- //
 function answerIsCorrect() {
-   document.getElementById(currentQuestion).style.backgroundColor = "#3F730A"
+   optionNode.style.backgroundColor = "#3F730A"
 }
 
 function answerIsWrong() {
